d0a4882e80d3c0b0038ea733c7a092db5ae73c28
Dan Frampton <dfrmptn@gmail.com>
2013-02-05 18:49:08 +0000

    Add exercises from section 1

---
+++ 02/ex2.1.1.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex2.1.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; ex2.1.1.rkt
+
+;; Find out whether DrScheme has operations for squaring a number, for computing
+;; the sine of an angle; and for determining the maximum of two numbers.
+
+(sqr 4)
+(sin 10)
+(max 2 3)
---
+++ 02/ex2.1.2.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex2.1.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; ex2.1.2.rkt
+
+;; Evaluate (sqrt 4), (sqrt 2), and (sqrt -1) in DrScheme. Then, find out
+;; whether DrScheme knows an operation for determining the tangent of an angle.
+
+(sqrt 4)
+(sqrt 2)
+(sqrt -1)
+(tan 30)
